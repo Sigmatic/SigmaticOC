2953d349db233d46d5e92a567a94f25b784d0d39
hisham <hishamabri@gmail.com>
2015-06-30 18:16:28 +1000

    Initial Project Commit

---
+++ SigmaticOC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SigmaticOC
+//
+//  Created by Hisham on 30/06/2015.
+//  Copyright (c) 2015 Sigmatic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SigmaticOC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SigmaticOC
+//
+//  Created by Hisham on 30/06/2015.
+//  Copyright (c) 2015 Sigmatic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
