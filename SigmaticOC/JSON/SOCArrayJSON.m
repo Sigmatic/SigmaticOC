f2109efcca615b17aa0a54c430635430552a8dd3
hisham <hishamabri@gmail.com>
2015-07-05 17:12:00 +1000

    Checking returned JSON data type is what is requested.

--- SigmaticOC/JSON/SOCArrayJSON.m
+++ SigmaticOC/JSON/SOCArrayJSON.m
@@ -5,12 +5,17 @@
 @implementation NSArray (SOCJSON)
 
 + (instancetype)fromJSON:(NSString *)json {
-    //TODO: check that the result is actually an array!
-    return [SOCJSONUtils jsonToObject:json options:[self jsonOptionForMutability]];
+    id hopefullyArray = [SOCJSONUtils jsonToObject:json options:[self jsonOptionForMutability]];
+    if (![hopefullyArray isKindOfClass:[NSDictionary class]]) {
+        NSString *reason = [NSString stringWithFormat:@"Requested data of type %@, but was %@",
+                                                      NSStringFromClass([self class]), NSStringFromClass([hopefullyArray class])];
+        NSException *e = [NSException exceptionWithName:@"SOCWrongJSONDataType" reason:reason userInfo:nil];
+        @throw e;
+    }
+    return hopefullyArray;
 }
 
 + (instancetype)safeFromJSON:(NSString *)json {
-    //TODO: check that the result is actually an array!
     @try {
         return [SOCJSONUtils jsonToObject:json options:[self jsonOptionForMutability]];
     } @catch (NSException *exception) {
--- SigmaticOC/JSON/SOCDictionaryJSON.m
+++ SigmaticOC/JSON/SOCDictionaryJSON.m
@@ -5,12 +5,17 @@
 @implementation NSDictionary (SOCJSON)
 
 + (instancetype)fromJSON:(NSString *)json {
-    //TODO: check that the result is actually a dictionary!
-    return [SOCJSONUtils jsonToObject:json options:[self jsonOptionForMutability]];
+    id hopefullyDictionary = [SOCJSONUtils jsonToObject:json options:[self jsonOptionForMutability]];
+    if (![hopefullyDictionary isKindOfClass:[NSDictionary class]]) {
+        NSString *reason = [NSString stringWithFormat:@"Requested data of type %@, but was %@",
+                        NSStringFromClass([self class]), NSStringFromClass([hopefullyDictionary class])];
+        NSException *e = [NSException exceptionWithName:@"SOCWrongJSONDataType" reason:reason userInfo:nil];
+        @throw e;
+    }
+    return hopefullyDictionary;
 }
 
 + (instancetype)safeFromJSON:(NSString *)json {
-    //TODO: check that the result is actually a dictionary!
     @try {
         return [SOCJSONUtils jsonToObject:json options:[self jsonOptionForMutability]];
     } @catch (NSException *exception) {
