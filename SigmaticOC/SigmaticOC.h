a4b5862d702fe51ef319752ae29ccc316d9f8d77
hisham <hishamabri@gmail.com>
2015-07-06 23:03:07 +1000

    Adding header files file

---
+++ SigmaticOC/SigmaticOC.h
@@ -0,0 +1,22 @@
+#import "SOCArrayExtension.h"
+#import "SOCArrayUtils.h"
+#import "SOCMutableArrayExtension.h"
+
+#import "SOCBoolSet.h"
+#import "SOCBoolUtils.h"
+
+#import "SOCDateUtils.h"
+
+#import "SOCMutableDictionaryExtension.h"
+
+#import "SOCArrayJSON.h"
+#import "SOCDictionaryJSON.h"
+#import "SOCJSONUtils.h"
+#import "SOCStringJSON.h"
+
+#import "SOCIntegerUtils.h"
+
+#import "SOCProtocolUtils.h"
+
+#import "SOCStringExtension.h"
+#import "SOCStringUtils.h"
