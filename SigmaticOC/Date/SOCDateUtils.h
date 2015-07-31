a5f43d67a93d1d0874ab9b54b90c280529135e51
hisham <hishamabri@gmail.com>
2015-08-01 01:28:44 +1000

    Adding some date utils

--- SigmaticOC/Date/SOCDateUtils.h
+++ SigmaticOC/Date/SOCDateUtils.h
@@ -1,5 +1,38 @@
 #import <Foundation/Foundation.h>
 
+extern const NSCalendarUnit ALL_CALENDAR_UNITS;
+extern const NSUInteger SECONDS_IN_MINUTE;
+extern const NSUInteger SECONDS_IN_HOUR;
+extern const NSUInteger SECONDS_IN_DAY;
+extern const NSUInteger SECONDS_IN_WEEK;
+extern const NSUInteger SECONDS_IN_YEAR;
 
 @interface SOCDateUtils : NSObject
+
++ (BOOL)isPast:(NSDate *)date;
+
++ (BOOL)isFuture:(NSDate *)date;
+
++ (BOOL)isDate:(NSDate *)date beforeDate:(NSDate *)otherDate;
+
++ (BOOL)isDate:(NSDate *)date afterDate:(NSDate *)otherDate;
+
++ (BOOL)isToday:(NSDate *)date;
+
++ (BOOL)isToday:(NSDate *)date inTimeZone:(NSTimeZone *)timeZone;
+
++ (NSDate *)startOfDay:(NSDate *)date;
+
++ (NSDate *)startOfDay:(NSDate *)date inTimeZone:(NSTimeZone *)timeZone;
+
++ (NSDate *)endOfDay:(NSDate *)date;
+
++ (NSDate *)endOfDay:(NSDate *)date inTimeZone:(NSTimeZone *)timeZone;
+
++ (BOOL)isDayInSameDay:(NSDate *)date asDate:(NSDate *)otherDate;
+
++ (BOOL)isDayInSameDay:(NSDate *)date asDate:(NSDate *)otherDate inTimeZone:(NSTimeZone *)timeZone;
+
++ (NSDate *)dateWithoutTime:(NSDate *)date;
+
 @end
--- SigmaticOC/Date/SOCDateUtils.m
+++ SigmaticOC/Date/SOCDateUtils.m
@@ -1,6 +1,99 @@
 #import "SOCDateUtils.h"
 
+const NSCalendarUnit ALL_CALENDAR_UNITS = (NSYearCalendarUnit | NSMonthCalendarUnit | NSDayCalendarUnit |
+        NSWeekCalendarUnit | NSHourCalendarUnit | NSMinuteCalendarUnit | NSSecondCalendarUnit | NSWeekdayCalendarUnit |
+        NSWeekdayOrdinalCalendarUnit);
+
+const NSUInteger SECONDS_IN_MINUTE = 60;
+const NSUInteger SECONDS_IN_HOUR = 3600;
+const NSUInteger SECONDS_IN_DAY = 86400;
+const NSUInteger SECONDS_IN_WEEK = 604800;
+const NSUInteger SECONDS_IN_YEAR = 31556926;
 
 @implementation SOCDateUtils
+
++ (BOOL)isPast:(NSDate *)date {
+    return [self isDate:date beforeDate:[NSDate date]];
+}
+
++ (BOOL)isFuture:(NSDate *)date {
+    return [self isDate:date afterDate:[NSDate date]];
+}
+
++ (BOOL)isDate:(NSDate *)date beforeDate:(NSDate *)otherDate {
+    return [date compare:otherDate] == NSOrderedAscending;
+}
+
++ (BOOL)isDate:(NSDate *)date afterDate:(NSDate *)otherDate {
+    return [date compare:otherDate] == NSOrderedDescending;
+}
+
++ (BOOL)isToday:(NSDate *)date {
+    return [self isToday:date inTimeZone:[NSTimeZone localTimeZone]];
+}
+
++ (BOOL)isToday:(NSDate *)date inTimeZone:(NSTimeZone *)timeZone {
+    return [self isDayInSameDay:date asDate:[NSDate date] inTimeZone:timeZone];
+}
+
++ (NSDate *)startOfDay:(NSDate *)date {
+    return [self startOfDay:date inTimeZone:[NSTimeZone localTimeZone]];
+}
+
++ (NSDate *)startOfDay:(NSDate *)date inTimeZone:(NSTimeZone *)timeZone {
+    NSCalendar *calendar = [NSCalendar currentCalendar];
+    calendar.timeZone = timeZone;
+    NSDateComponents *components = [calendar components:ALL_CALENDAR_UNITS fromDate:date];
+    components.timeZone = timeZone;
+    components.hour = components.minute = components.second = 0;
+    NSDate *startDate = [calendar dateFromComponents:components];
+    return startDate;
+}
+
++ (NSDate *)endOfDay:(NSDate *)date {
+    return [self endOfDay:date inTimeZone:[NSTimeZone localTimeZone]];
+}
+
++ (NSDate *)endOfDay:(NSDate *)date inTimeZone:(NSTimeZone *)timeZone {
+    NSCalendar *calendar = [NSCalendar currentCalendar];
+    calendar.timeZone = timeZone;
+    NSDateComponents *components = [calendar components:ALL_CALENDAR_UNITS fromDate:date];
+    components.timeZone = timeZone;
+    components.hour = 23;
+    components.minute = 59;
+    components.second = 59;
+    NSDate *endDate = [calendar dateFromComponents:components];
+    return endDate;
+}
+
++ (BOOL)isDayInSameDay:(NSDate *)date asDate:(NSDate *)otherDate {
+    return [self isDayInSameDay:date asDate:otherDate inTimeZone:[NSTimeZone localTimeZone]];
+}
+
++ (BOOL)isDayInSameDay:(NSDate *)date asDate:(NSDate *)otherDate inTimeZone:(NSTimeZone *)timeZone {
+    NSCalendar *calendar = [NSCalendar currentCalendar];
+    calendar.timeZone = timeZone;
+    NSCalendarUnit calendarUnits = NSYearCalendarUnit | NSMonthCalendarUnit | NSDayCalendarUnit;
+    NSDateComponents *components = [calendar components:calendarUnits fromDate:date];
+    components.timeZone = timeZone;
+    NSDateComponents *dateComponents = [calendar components:calendarUnits fromDate:otherDate];
+    dateComponents.timeZone = timeZone;
+    if (components.year != dateComponents.year) {
+        return NO;
+    }
+    if (components.month != dateComponents.month) {
+        return NO;
+    }
+    return components.day == dateComponents.day;
+}
+
++ (NSDate *)dateWithoutTime:(NSDate *)date {
+    NSCalendar *calendar = [NSCalendar currentCalendar];
+    calendar.timeZone = [NSTimeZone localTimeZone];
+    NSCalendarUnit calendarUnits = NSYearCalendarUnit | NSMonthCalendarUnit | NSDayCalendarUnit;
+    NSDateComponents *components = [[NSCalendar currentCalendar] components:calendarUnits fromDate:date];
+    components.timeZone = [NSTimeZone localTimeZone];
+    return [calendar dateFromComponents:components];
+}
 
 @end
