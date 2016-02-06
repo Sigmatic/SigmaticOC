320f7b926307867c7324c44cf61d0052a9ae284a
Hisham <hisham@alabri.co>
2016-02-06 14:32:28 +1000

    Adding documentation.

--- SigmaticOC/Number/SOCNumberMultiples.h
+++ SigmaticOC/Number/SOCNumberMultiples.h
@@ -12,13 +12,45 @@ typedef NS_ENUM(NSUInteger, SOCNumberMultiple) {
     SOCMultipleYotta = 9
 };
 
+/**
+ The decimal multiplier uses 1000 when stepping to the next multiplier unit
+ */
 static const NSUInteger DECIMAL_MULTIPLIER = 1000;
+
+/**
+ The binary multiplier uses 1024 instead of 1000 when stepping to the next multiplier unit
+ */
 static const NSUInteger BINARY_MULTIPLIER = 1024;
 
+/**
+ The `SOCNumberMultiples` class provides easy numbers multiplication between different units.
+ */
 @interface SOCNumberMultiples : NSObject
 
+
+
+///------------------------------------
+/// @name Multiply
+///------------------------------------
+
+/**
+ Multiply an amount from a unit type to a unit type using decimal conversion.
+
+ @param amount The amount to convert.
+ @param fromType The type the amount is in.
+ @param toType The type desired as output.
+ @return The value after conversion.
+ */
 + (double)binaryConvert:(double)amount from:(SOCNumberMultiple)fromType to:(SOCNumberMultiple)toType;
 
+/**
+ Multiply an amount from a unit type to a unit type using digital conversion.
+
+ @param amount The amount to convert.
+ @param fromType The type the amount is in.
+ @param toType The type desired as output.
+ @return The value after conversion.
+ */
 + (double)decimalConvert:(double)amount from:(SOCNumberMultiple)fromType to:(SOCNumberMultiple)toType;
 
 @end
